84f5b320c99b708e5ca815ab6a0d7e42d4ae171e
nailseong <nnis9857@gmail.com>
2022-05-26 01:07:16 +0900

    fix(PathServiceTest): 실패하는 테스트 코드 수정

--- src/test/java/wooteco/subway/service/PathServiceTest.java
+++ src/test/java/wooteco/subway/service/PathServiceTest.java
@@ -5,19 +5,30 @@ import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import wooteco.subway.domain.Distance;
 import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Name;
-import wooteco.subway.domain.Path;
 import wooteco.subway.domain.Section;
 import wooteco.subway.domain.Sections;
 import wooteco.subway.domain.Station;
 import wooteco.subway.domain.fare.Fare;
+import wooteco.subway.domain.fare.strategy.discount.BabyDiscountStrategy;
+import wooteco.subway.domain.fare.strategy.discount.ChildDiscountStrategy;
+import wooteco.subway.domain.fare.strategy.discount.DefaultDiscountStrategy;
+import wooteco.subway.domain.fare.strategy.discount.DiscountStrategy;
+import wooteco.subway.domain.fare.strategy.discount.DiscountStrategyFactory;
+import wooteco.subway.domain.fare.strategy.discount.SeniorDiscountStrategy;
+import wooteco.subway.domain.fare.strategy.discount.TeenagerDiscountStrategy;
+import wooteco.subway.domain.fare.strategy.extrafare.ExtraFareStrategy;
+import wooteco.subway.domain.fare.strategy.extrafare.ExtraFareStrategyFactory;
+import wooteco.subway.domain.fare.strategy.extrafare.TenExtraFareStrategy;
 
 class PathServiceTest extends ServiceTest {
 
@@ -30,24 +41,41 @@ class PathServiceTest extends ServiceTest {
     @Mock
     private StationService stationService;
 
-    @ParameterizedTest
-    @DisplayName("출발역과 도착역의 최단 경로에 대한 정보를 조회한다.")
-    @CsvSource(value = {"6:700", "12:700", "13:1120", "18:1120", "19:1750"}, delimiter = ':')
-    void Find(final int age, final int expectedFare) {
-        // given
-        final Station gangnam = new Station(1L, "강남역");
-        final Station yeoksam = new Station(2L, "역삼역");
-        final Station seolleung = new Station(3L, "선릉역");
-        final Station samsung = new Station(4L, "삼성역");
+    @Mock
+    private ExtraFareStrategyFactory extraFareStrategyFactory;
 
-        final Station seoulForest = new Station(5L, "서울숲역");
-        final Station wangsimni = new Station(6L, "왕십리역");
+    @Mock
+    private DiscountStrategyFactory discountStrategyFactory;
 
-        final Station yacksu = new Station(7L, "약수역");
-        final Station geumho = new Station(8L, "금호역");
-        final Station oksu = new Station(9L, "옥수역");
+    private Station gangnam;
+    private Station yeoksam;
+    private Station seolleung;
+    private Station samsung;
+    private Station seoulForest;
+    private Station wangsimni;
+    private Station yacksu;
+    private Station geumho;
+    private Station oksu;
 
-        Line greenLine = new Line(1L, new Name("2호선"), "green", 100);
+    private Line greenLine;
+    private Line yellowLine;
+    private Line orangeLine;
+
+    @BeforeEach
+    void setUpDate() {
+        gangnam = new Station(1L, "강남역");
+        yeoksam = new Station(2L, "역삼역");
+        seolleung = new Station(3L, "선릉역");
+        samsung = new Station(4L, "삼성역");
+
+        seoulForest = new Station(5L, "서울숲역");
+        wangsimni = new Station(6L, "왕십리역");
+
+        yacksu = new Station(7L, "약수역");
+        geumho = new Station(8L, "금호역");
+        oksu = new Station(9L, "옥수역");
+
+        greenLine = new Line(1L, new Name("2호선"), "green", 100);
         final Section greenSectionA = new Section(greenLine, gangnam, yeoksam, new Distance(10));
         final Section greenSectionB = new Section(greenLine, yeoksam, seolleung, new Distance(7));
         final Section greenSectionC = new Section(greenLine, seolleung, samsung, new Distance(11));
@@ -57,7 +85,7 @@ class PathServiceTest extends ServiceTest {
                 greenSectionC
         )));
 
-        Line yellowLine = new Line(2L, new Name("수인분당선"), "yellow", 300);
+        yellowLine = new Line(2L, new Name("수인분당선"), "yellow", 300);
         final Section yellowSectionA = new Section(yellowLine, seolleung, seoulForest, new Distance(3));
         final Section yellowSectionB = new Section(yellowLine, seoulForest, wangsimni, new Distance(8));
         yellowLine = yellowLine.addSections(new Sections(List.of(
@@ -65,14 +93,37 @@ class PathServiceTest extends ServiceTest {
                 yellowSectionB
         )));
 
-        Line orangeLine = new Line(3L, new Name("3호선"), "orange", 500);
+        orangeLine = new Line(3L, new Name("3호선"), "orange", 500);
         final Section orangeSectionA = new Section(orangeLine, yacksu, geumho, new Distance(12));
         final Section orangeSectionB = new Section(orangeLine, geumho, oksu, new Distance(6));
         orangeLine = orangeLine.addSections(new Sections(List.of(
                 orangeSectionA,
                 orangeSectionB
         )));
+    }
 
+    @Test
+    @DisplayName("출발역과 도착역의 최단 경로에 포함된 역들을 조회한다.")
+    void Find_Stations() {
+        // given
+        prepareBeforeFindPath(new DefaultDiscountStrategy());
+
+        final List<Station> expected = List.of(
+                gangnam,
+                yeoksam,
+                seolleung,
+                seoulForest
+        );
+
+        // when
+        final List<Station> actual = pathService.find(gangnam.getId(), seoulForest.getId(), 25)
+                .getStations();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private void prepareBeforeFindPath(final DiscountStrategy discountStrategy) {
         given(stationService.findById(any(Long.class)))
                 .willReturn(gangnam)
                 .willReturn(seoulForest);
@@ -85,21 +136,93 @@ class PathServiceTest extends ServiceTest {
         given(lineService.findAll())
                 .willReturn(lines);
 
-        final List<Station> expectedStations = List.of(
-                gangnam,
-                yeoksam,
-                seolleung,
-                seoulForest
-        );
-        final Distance expectedDistance = new Distance(20);
-        final Path expected = new Path(
-                expectedStations,
-                expectedDistance,
-                Fare.from(expectedFare - 1250)
-        );
+        final ExtraFareStrategy extraFareStrategy = new TenExtraFareStrategy();
+        given(extraFareStrategyFactory.findStrategyBy(any(Distance.class)))
+                .willReturn(extraFareStrategy);
+
+        given(discountStrategyFactory.findStrategyBy(any(Integer.class)))
+                .willReturn(discountStrategy);
+    }
+
+    @ParameterizedTest
+    @DisplayName("아기 승객의 출발역과 도착역의 최단 경로의 요금을 조회한다.")
+    @ValueSource(ints = {1, 5})
+    void Find_FareOfBaby(final int age) {
+        // given
+        prepareBeforeFindPath(new BabyDiscountStrategy());
+
+        final Fare expected = Fare.from(-1250);
 
         // when
-        final Path actual = pathService.find(gangnam.getId(), seoulForest.getId(), age);
+        final Fare actual = pathService.find(gangnam.getId(), seoulForest.getId(), age)
+                .getFare();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("어린이 승객의 출발역과 도착역의 최단 경로의 요금을 조회한다.")
+    @ValueSource(ints = {6, 12})
+    void Find_FareOfChild(final int age) {
+        // given
+        prepareBeforeFindPath(new ChildDiscountStrategy());
+
+        final Fare expected = Fare.from(-550);
+
+        // when
+        final Fare actual = pathService.find(gangnam.getId(), seoulForest.getId(), age)
+                .getFare();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("청소년 승객의 출발역과 도착역의 최단 경로의 요금을 조회한다.")
+    @ValueSource(ints = {13, 18})
+    void Find_FareOfTeenager(final int age) {
+        // given
+        prepareBeforeFindPath(new TeenagerDiscountStrategy());
+
+        final Fare expected = Fare.from(-130);
+
+        // when
+        final Fare actual = pathService.find(gangnam.getId(), seoulForest.getId(), age)
+                .getFare();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("일반 승객의 출발역과 도착역의 최단 경로의 요금을 조회한다.")
+    @ValueSource(ints = {19, 64})
+    void Find_FareOfDefault(final int age) {
+        // given
+        prepareBeforeFindPath(new DefaultDiscountStrategy());
+
+        final Fare expected = Fare.from(500);
+
+        // when
+        final Fare actual = pathService.find(gangnam.getId(), seoulForest.getId(), age)
+                .getFare();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("노인 승객의 출발역과 도착역의 최단 경로의 요금을 조회한다.")
+    void Find_FareOfSenior() {
+        // given
+        prepareBeforeFindPath(new SeniorDiscountStrategy());
+
+        final Fare expected = Fare.from(-1250);
+
+        // when
+        final Fare actual = pathService.find(gangnam.getId(), seoulForest.getId(), 65)
+                .getFare();
 
         // then
         assertThat(actual).isEqualTo(expected);
